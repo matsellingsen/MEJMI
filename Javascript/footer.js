cec03a590dd0e62b1434e2a9b4257ed4c23a5680
Edvard Bjørnevik <edvardbj@stud.ntnu.no>
2020-10-13 15:18:15 +0200

    Fikset link til face, insta og Linkedin. Ikke twitter

--- Javascript/footer.js
+++ Javascript/footer.js
@@ -3,7 +3,7 @@ var i;
 var out = document.getElementById("foot");
 var args = ["Brobekkveien 80A, 0582 TRD", "910 03 270 | 922 16 042", "post@mejmimedia.no", "© 2020 | MEJMI • All rights reserved"];
 var kilde = ["./Images/FACE.jpg", "./Images/INSTA.jpg", "./Images/LINKEDIN.jpg", "./Images/TWITTER.jpg"];
-var referanse = ["https://www.facebook.com", "https://www.instagram.com", "https://www.linkedin.com", "https://www.twitter.com"];
+var referanse = ["https://www.facebook.com/MEJMI-webdesign-113757423838059", "https://www.instagram.com/mejmi_webdesign/", "https://www.linkedin.com/in/mejmi-webdesign-2665841b8/", "https://www.twitter.com"];
 function displayArgs() {
     "use strict";
     for (i = 0; i < args.length -1; i++) {
