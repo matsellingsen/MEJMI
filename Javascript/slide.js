d1381fc6c5bfce41201ae80e761e84c2aa67c0e0
Jakob Keller <jekeller@stud.ntnu.no>
2020-10-25 18:14:42 +0100

    Oppdaters CSS, hovedside og lagt til slidefunksjon

---
+++ Javascript/slide.js
@@ -0,0 +1,19 @@
+/*Hentet koden fra https://stackoverflow.com/questions/30388118/trying-to-make-multiple-background-images-cycle-through-a-slideshow-with-css-and */
+
+var i = 1;
+setInterval(function() { 
+    e = document.getElementById('hovedbilde');
+    switch (i) {
+        case 0:
+            e.style.backgroundPosition = 'center top, center top, center top';
+            break;
+        case 1:
+            e.style.backgroundPosition = window.innerWidth + 'px top, center top, center top';
+            break;
+        case 2:
+            e.style.backgroundPosition = window.innerWidth + 'px top, ' + window.innerWidth + 'px top, center top';
+            break;
+    }
+    i++;    
+    if (i > 2) i = 0;   
+}, 5000);
