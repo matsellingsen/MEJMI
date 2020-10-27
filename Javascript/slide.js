2681afcd82ca38a564c157a2685f3f37495f172a
Mathias Schulstok <mathiasschulstok@dhcp-10-22-12-11.wlan.ntnu.no>
2020-10-27 18:17:34 +0100

    alt er jævlig bra

--- Javascript/slide.js
+++ Javascript/slide.js
@@ -4,13 +4,13 @@ setInterval(function() {
     console.log(bilde)
     switch (i) {
         case 0:
-            bilde.style.backgroundImage = "url('Images/referanse1.jpg')"
+            bilde.style.backgroundImage = "url('Images/referanser/referanse1.jpg')"
             break
         case 1:
-            bilde.style.backgroundImage = "url('Images/referanse2.jpg')"
+            bilde.style.backgroundImage = "url('Images/referanser/referanse2.jpg')"
             break
         case 2:
-            bilde.style.backgroundImage = "url('Images/referanse3.jpg')"
+            bilde.style.backgroundImage = "url('Images/referanser/referanse3.jpg')"
     }
     i++
     if (i > 2) {
