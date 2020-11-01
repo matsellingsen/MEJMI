42d5a5468781e903094eb123263a5ed1ed1f6382
Mathias Schulstok <mathiasschulstok@dhcp-10-22-15-62.wlan.ntnu.no>
2020-11-01 17:54:06 +0100

    lagde hover over tjenester, men trenger transition

---
+++ Javascript/hovertjenester.js
@@ -0,0 +1,22 @@
+let nDesign = document.getElementById("tjenester_nDesign");
+let gprofil = document.getElementById("tjenester_gProfil");
+let seo = document.getElementById("tjenester_SEO");
+
+nDesign.addEventListener("mouseover", større);
+gprofil.addEventListener("mouseover", større);
+seo.addEventListener("mouseover", større);
+
+nDesign.addEventListener("mouseout", mindre);
+gprofil.addEventListener("mouseout", mindre);
+seo.addEventListener("mouseout", mindre);
+
+
+
+function større() {
+    this.style.transform = "scale(1.06)";
+}
+
+function mindre() {
+    this.style.transform = "scale(1)";
+}
+
