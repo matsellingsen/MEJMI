21ded421d0fc52a547104ca128c14614b33b3174
Mathias Schulstok <mathiasschulstok@dhcp-10-22-15-62.wlan.ntnu.no>
2020-11-02 18:12:24 +0100

    norske variabelnavn

--- Javascript/hovertjenester.js
+++ Javascript/hovertjenester.js
@@ -2,9 +2,9 @@ let nDesign = document.getElementById("tjenester_nDesign");
 let gprofil = document.getElementById("tjenester_gProfil");
 let seo = document.getElementById("tjenester_SEO");
 
-nDesign.addEventListener("mouseover", større);
-gprofil.addEventListener("mouseover", større);
-seo.addEventListener("mouseover", større);
+nDesign.addEventListener("mouseover", storre);
+gprofil.addEventListener("mouseover", storre);
+seo.addEventListener("mouseover", storre);
 
 nDesign.addEventListener("mouseout", mindre);
 gprofil.addEventListener("mouseout", mindre);
@@ -12,7 +12,7 @@ seo.addEventListener("mouseout", mindre);
 
 
 
-function større() {
+function storre() {
     this.style.transform = "scale(1.06)";
     this.style.transition = "0.3s";
 }
